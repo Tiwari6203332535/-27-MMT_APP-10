f564d645a61b564be78cd1aaa1c2752259a4bed6
happy <tiwari95874@gmail.com>
2024-09-07 07:45:33 +0530

    first commit

--- src/main/java/in/happy/service/MMTServiceImpl.java
+++ src/main/java/in/happy/service/MMTServiceImpl.java
@@ -3,6 +3,8 @@ package in.happy.service;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import in.happy.request.Passenger;
 import in.happy.response.Ticket;
@@ -14,17 +16,35 @@ public class MMTServiceImpl implements MMTService {
 	private String  GET_TICKET_URL="http://13.232.253.164.164:8080/ticket/{ticketNum}";
 	@Override
 	public Ticket bookTicket(Passenger passenger) {
-		RestTemplate rt = new RestTemplate();
-		ResponseEntity<Ticket> responseEntity = rt.postForEntity(BOOK_TICKET_URL, passenger, Ticket.class);
-		Ticket ticket = responseEntity.getBody();
+		WebClient webClient = WebClient.create();
+		Ticket ticket = webClient.post()
+		                     .uri(BOOK_TICKET_URL)
+                       		.bodyValue(passenger)
+                     		.retrieve()
+                    		.bodyToMono(Ticket.class)
+                    		.block();
+		
+		
+//		RestTemplate rt = new RestTemplate();
+//		ResponseEntity<Ticket> responseEntity = rt.postForEntity(BOOK_TICKET_URL, passenger, Ticket.class);
+//		Ticket ticket = responseEntity.getBody();
 		return ticket;
 	}
 
 	@Override
-	public Ticket getTicketByNum(Integer ticketInteger) {
-		RestTemplate rt = new RestTemplate();
-		ResponseEntity<Ticket> responseEntity = rt.getForEntity(BOOK_TICKET_URL, Ticket.class, ticketInteger);
-		Ticket ticket = responseEntity.getBody();
+	public Ticket getTicketByNum(Integer ticketNumber) {
+		//get the object of webclient(impl class)
+		WebClient webClient = WebClient.create();
+		//send get request and map request to ticket obj
+		Ticket ticket = webClient.get()
+		           .uri(GET_TICKET_URL,ticketNumber)
+		           .retrieve()
+		           .bodyToMono(Ticket.class)
+		           .block();//sync call=after request wait for response
+		
+//		RestTemplate rt = new RestTemplate();
+//		ResponseEntity<Ticket> responseEntity = rt.getForEntity(BOOK_TICKET_URL, Ticket.class, ticketInteger);
+//		Ticket ticket = responseEntity.getBody();
 		return ticket;
 	}
 
